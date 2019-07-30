468bc84b2d1806a533d21a4b4445977096344b18
Nicolas <njtret0815@gmail.com>
2019-07-30 17:06:38 -0400

    change set route button

--- Home/index.js
+++ Home/index.js
@@ -1,14 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Text, View } from 'react-native';
+import { Button, Text, View, TouchableOpacity } from 'react-native';
 import RouteList from './RouteList';
 import { fetchRoutes } from '../actions';
-
-import { fakeRoutes } from '../support/fakeRoutes';
+import { SCREEN_BACKGROUND_COLOR, ROUTE_BUTTON_COLOR } from '../style';
 
 const style = {
-  flex: 1,
-  padding: 16,
-  backgroundColor: 'rgba(194, 233, 251, .25)'
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: SCREEN_BACKGROUND_COLOR
+  },
+  button: {
+    height: 40,
+    paddingLeft: 16,
+    paddingRight: 16,
+    marginBottom: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    borderRadius: 5,
+    backgroundColor: ROUTE_BUTTON_COLOR
+  },
+  buttonText: {
+    color: 'white'
+  }
 };
 
 const Home = (props) => {
@@ -18,8 +33,10 @@ const Home = (props) => {
   }, []);
 
   return (
-    <View style={style}>
-      <Button onPress={() => props.navigation.navigate('SetRoute')} title='Set Route' />
+    <View style={style.container}>
+      <TouchableOpacity style={style.button} onPress={() => props.navigation.navigate('SetRoute')}>
+        <Text style={style.buttonText}>Set Route</Text>
+      </TouchableOpacity>
       <RouteList routes={routes} />
     </View>
   );
